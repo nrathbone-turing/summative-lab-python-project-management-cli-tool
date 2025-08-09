70b05b29a7b8c409594dcb0ceca64b23fd5850d0
Nick Rathbone <nrathbone@joinhandshake.com>
2025-08-09 17:05:24 -0600

    Fix imports from json_store

--- services/app.py
+++ services/app.py
@@ -1,5 +1,5 @@
 from . import app as _   # makes package importable
-from ..store.json_store import JsonStore
+from ..store.json_store import save_all, load_all
 from ..models.user import User
 from ..models.project import Project
 from ..models.task import Task
