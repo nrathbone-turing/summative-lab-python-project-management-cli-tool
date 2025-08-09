de048738d6915d01abc5dcfc91ea72b439d7159f
Nick Rathbone <nrathbone@joinhandshake.com>
2025-08-09 13:53:33 -0600

    Create placeholder classes for main app and json_store

---
+++ package/services/app.py
@@ -0,0 +1,8 @@
+from . import app as _   # makes package importable
+from ..store.json_store import JsonStore
+from ..models.user import User
+from ..models.project import Project
+from ..models.task import Task
+
+class App:
+    pass
---
+++ package/store/json_store.py
@@ -0,0 +1,7 @@
+import json
+from pathlib import Path
+
+DEFAULT_PATH = Path(__file__).resolve().parents[2] / "data" / "db.json"
+
+class JsonStore:
+    pass
